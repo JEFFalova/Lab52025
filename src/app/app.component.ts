6db85c500cda05d32aaf4d5b2a394cf88eb76fdf
JEFFalova <g00424590@atu.ie>
2025-03-11 23:43:14 +0000

    Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DataService } from './services/data.service';
-import { CommonModule } from '@angular/common';
+import { DataService } from './services/data.service';// DataService is a service that you use to fetch data (e.g., student and weather data)
+import { CommonModule } from '@angular/common';// CommonModule provides common directives like ngIf and ngFor
 
 @Component({
   selector: 'app-root',
@@ -10,22 +10,27 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit{
-
-  students:any[]=[];
-  weather : any[]=[];
-  temperature: any = "";
+// Declare variables to hold the student and weather data
+  students:any[]=[];// array for student data
+  weather : any[]=[];// array for weather data
+  temperature: any = "";// A variable to store the temperature value (in Celsius after conversion)
 
   constructor(private dataService:DataService){}
   
+  // ngOnInit lifecycle hook to initialize data when the component is created
   ngOnInit(): void {
+    // Fetch student data from the DataService when the component initializes
     this.dataService.getStudentData().subscribe(
       (data)=>{
+         // On successful response, assign the student data to the 'students' array
+        // Assuming the 'students' data comes in the format of an object with a 'students' property
       //console.log(data);
       this.students = data.students;
       //this.adress = data.adress;
       }
     );
 
+    // Fetch weather data from the DataService when the component initializes
     this.dataService.getWeatherData().subscribe(
       (data)=>{
         this.weather = data.weather;
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,19 +1,25 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';// Injectable decorator is used to mark this class as one that can be injected as a service
+import { HttpClient } from '@angular/common/http';// HttpClient is used to make HTTP requests
+import { Observable } from 'rxjs'; // Observable is the return type of HTTP requests in Angular
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-
+  // Injecting HttpClient into the constructor to enable making HTTP requests
   constructor(private httpClient:HttpClient) { }
 
+   // Method to get student data from a remote server using an HTTP GET request
   getStudentData():Observable<any>{
+        // Makes an HTTP GET request to the specified URL and returns the result as an Observable
     return this.httpClient.get('https://www.jsonblob.com/api/jsonblob/1347498620747964416')
   }
 
+  // Method to get weather data from OpenWeather API
   getWeatherData():Observable<any>{
+    // Makes an HTTP GET request to the OpenWeather API with a hardcoded city (Galway)
+    // The 'appid' is the API key required to access the service
     return this.httpClient.get('https://api.openweathermap.org/data/2.5/weather?q=Galway&appid=6a66416403ed8e5e6e762cb8c261f303')
   }
 }
